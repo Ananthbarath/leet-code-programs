16cf4e8da999b4b4dd1f3389e10272f11f4393ed
Ananth <92591290+Ananthbarath@users.noreply.github.com>
2024-11-04 13:58:16 +0530

    Create 1280. Students and Examinations.sql

---
+++ 1280. Students and Examinations.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select s.student_id, s.student_name, su.subject_name,(SELECT COUNT(1) 
+	FROM Examinations 
+	WHERE student_id = s.student_id 
+	AND subject_name = su.subject_name) AS attended_exams
+FROM Students s
+JOIN Subjects su
+ ORDER BY s.student_id, su.subject_name
